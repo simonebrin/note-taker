ee93d3779d92e0dc0edf3fdd7cf5306b3141e752
Simone Brin <simonebrin@gmail.com>
2022-09-01 12:03:16 -0400

    adding post and delete routes

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -7,12 +7,33 @@ const notes  = require('../../db/db.json');
 
 router.get('/notes', (req, res) => {
   let currentNotes = notes;
-  req.json(currentNotes);
+  res.json(currentNotes);
 });
 
 router.get('/notes/:id', (req, res) => {
-  
+  let currentNotes = notes;
+  res.json(currentNotes[req.params.id-1])
 });
 
+router.post('/notes', (req, res) => {
+  let currentNotes = notes;
+  currentNotes.push(req.body);
+  res.json(currentNotes);
+
+});
+
+router.delete('/notes/:id', (req, res) => {
+  let currentNotes = notes;
+  currentNotes.splice(req.params.id-1, 1)
+  res.json(currentNotes)
+})
 
 module.exports = router;
+
+const newNote = {
+  title: "New note to save",
+  text: "new notes text"
+}
+
+const arrayOfAllNotes = []
+arrayOfAllNotes.push(newNote)
